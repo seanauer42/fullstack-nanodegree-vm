55f0714da13c9ee7a2eeb549110cfa7178cf5a91
Seanauer42 <yodaman40@gmail.com>
2020-07-01 03:19:29 -0400

    finished adding to this database, it was sloppy. learned about updating and deleting data in the terminal

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lex/dev/full stack foundations/fullstack/venv"
+set -gx VIRTUAL_ENV "/home/lex/Documents/dev/full stack foundations/fullstack/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
